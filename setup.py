ac48c57141679b6f19ff2dbc2c84df00bd5b3e4b
JULEN ALVARO ESPINO <jalvaro@caf.net>
2024-03-03 20:47:25 +0100

    arreglado tree.py no filtraba bien

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Usar with para asegurar que el archivo se cierre correctamente
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='javtools',
+    version='0.1',
+    packages=find_packages(),
+    description='Herramientas personalizadas para mi uso.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Indicar el tipo de contenido del long_description
+    install_requires=[
+        'pyperclip==1.8.2', 
+    ],
+)
